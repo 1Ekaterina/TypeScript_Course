fe1c90f1aa40f0eba7ea5f1e3b150fc4d7056ba2
Ekaterina <kh.katerina@bk.ru>
2022-01-26 20:22:32 +0300

    ls6

--- src/lodash-debounce.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'lodash.debounce' {
-    export interface DebounceSettings {
-        leading?: boolean | undefined;
-        maxWait?: number | undefined;
-        trailing?: boolean | undefined;
-    }
-    export function debounce(func: Function, wait?: number, options?: DebounceSettings): void
-}
